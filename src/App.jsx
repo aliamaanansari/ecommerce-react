5759ee3de06d9d98f04a82c76714d28d7b10b1d8
Ali aman Ansari <aliamaan071@gmail.com>
2022-04-18 03:15:32 +0530

    feat: added auth pages (singup/login/forgot-password)

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,13 @@
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import Navbar from "./components/Navbar";
 import CategoryPage from "./pages/CategoryPage";
+import Forget from "./pages/Forget";
 import HomePage from "./pages/HomePage";
+import Login from "./pages/LoginPage";
 import NotFoundPage from "./pages/NotFoundPage";
 import ProductPage from "./pages/ProductPage";
 import ProductsPage from "./pages/ProductsPage";
+import Register from "./pages/Register";
 
 function App() {
   return (
@@ -13,6 +16,9 @@ function App() {
       <Routes>
         <Route path="/products" element={<ProductsPage />} />
         <Route path="/" element={<HomePage />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/forgot-password" element={<Forget />} />
+        <Route path="/register" element={<Register />} />
         <Route path="/product/:productId" element={<ProductPage />} />
         <Route path="/category/:categoryId" element={<CategoryPage />} />
         <Route path="*" element={<NotFoundPage />} />
